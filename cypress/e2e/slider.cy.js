94eb302273793a338d4fafbe8794cbc4ce41735d
Kraisu0 <krzysztofpopiela@poczta.fm>
2025-03-25 20:33:13 +0100

    Added hotjar script, Added test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -7,6 +7,24 @@ describe('Swiper Gallery Test', function () {
 });
 
 describe('Swiper Gallery Test', function () {
+  it('Checks if first slide contains "Rome"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
